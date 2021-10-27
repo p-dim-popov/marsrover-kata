15ba22a3e7cc9f9ca46f7a7dfefff8eb418ae752
Peter Popov <p.dim.popov@gmail.com>
2021-10-27 23:22:26 +0300

    feat(Point): simple point with x, y

--- src/features/Grid.ts
+++ src/features/Grid.ts
@@ -1,9 +1,19 @@
+import {Point} from "./Point";
+
 export class Grid {
     public readonly rows: number;
     public readonly cols: number;
+    public readonly obstacles: Point[];
 
-    constructor(rows: number, cols?: number) {
+    constructor(rows: number, cols?: number | Point[], obstacles?: Point[]) {
         this.rows = rows;
-        this.cols = cols ?? rows;
+        this.cols = rows;
+        this.obstacles = obstacles ?? [];
+
+        if (typeof cols === "number") {
+            this.cols = cols;
+        } else if (Array.isArray(cols)) {
+            this.obstacles = cols;
+        }
     }
 }
--- src/features/MarsRover.test.ts
+++ src/features/MarsRover.test.ts
@@ -1,18 +1,19 @@
 import MarsRover, {CommandType} from "./MarsRover";
 import {Grid} from "./Grid";
 import {Coordinates} from "./Coordinates";
+import {Point} from "./Point";
 
-const basicGrid = new Grid(10);
+const grid = new Grid(10, [ new Point(1, 3) ]);
 
 describe("MarsRover", () => {
     describe("execute", () => {
         it("should throw when command is not valid", function () {
-            const marsRover = new MarsRover(basicGrid);
+            const marsRover = new MarsRover(grid);
             expect(() => marsRover.execute("")).toThrow();
         });
 
         it("should return valid coordinates", function () {
-            const marsRover = new MarsRover(basicGrid);
+            const marsRover = new MarsRover(grid);
             expect(() => Coordinates.parse(marsRover.execute("MMM"))).not.toThrow();
         });
 
@@ -21,8 +22,9 @@ describe("MarsRover", () => {
             ["MMM", "3:0:N"],
             [[CommandType.Move, CommandType.Move], "2:0:N"],
             ["MMMMMMMMMM", "0:0:N"],
+            [""]
         ])('should move as expected (%s) -> (%s)', function (commands: string | CommandType[], expectedCoordinates) {
-            const marsRover = new MarsRover(basicGrid);
+            const marsRover = new MarsRover(grid);
             expect(marsRover.execute(commands)).toEqual(expectedCoordinates);
         });
     })
