e6a38c1cbb814e5b6d3c20e05471d507b9402e34
Peter Popov <p.dim.popov@gmail.com>
2021-10-27 18:05:05 +0300

    feat(MarsRover): implement basic movement

--- src/features/MarsRover.test.ts
+++ src/features/MarsRover.test.ts
@@ -2,16 +2,25 @@ import MarsRover from "./MarsRover";
 import {Grid} from "./Grid";
 import {Coordinates} from "./Coordinates";
 
+const basicGrid = new Grid(10);
+
 describe("MarsRover", () => {
     describe("move", () => {
         it("should throw when command is not valid", function () {
-            const marsRover = new MarsRover(new Grid(10));
+            const marsRover = new MarsRover(basicGrid);
             expect(() => marsRover.move("")).toThrow();
         });
 
         it("should return valid coordinates", function () {
-            const marsRover = new MarsRover(new Grid(10));
+            const marsRover = new MarsRover(basicGrid);
             expect(() => Coordinates.parse(marsRover.move("MMM"))).not.toThrow();
+        });
+
+        it.each([
+            ["M", "1:0:N"],
+        ])('should work as expected', function (commands: string, expectedCoordinates) {
+            const marsRover = new MarsRover(basicGrid);
+            expect(marsRover.move(commands)).toEqual(expectedCoordinates);
         });
     })
 })
--- src/features/MarsRover.ts
+++ src/features/MarsRover.ts
@@ -1,5 +1,5 @@
 import {Grid} from "./Grid";
-import {Coordinates} from "./Coordinates";
+import {Coordinates, DirectionType} from "./Coordinates";
 
 export interface IMarsRover {
     move(command: string): string;
@@ -9,12 +9,44 @@ export interface IMarsRoverConstructor {
     new(grid: Grid): IMarsRover;
 }
 
+export enum CommandType {
+    Move = "M",
+    RotateLeft = "L",
+    RotateRight = "R",
+}
+
 export const MarsRover: IMarsRoverConstructor = class implements IMarsRover {
     private coordinates = new Coordinates("0:0:N");
     constructor(private readonly grid: Grid) {}
-    move(command: string): string {
-        if (!command) {
+    move(commands: string | CommandType[]): string {
+        if (!commands) {
             throw new Error("Command is not valid!");
+        }
+
+        for (const _ of typeof commands === "string" ? commands.split(""): commands) {
+            const command = _ as CommandType;
+            switch (command) {
+                case CommandType.Move:
+                    switch (this.coordinates.direction) {
+                        case DirectionType.East:
+                        case DirectionType.West:
+                            this.coordinates.col += 1;
+                            break;
+                        case DirectionType.North:
+                        case DirectionType.South:
+                            this.coordinates.row += 1;
+                            break;
+                        default:
+                            throw new Error("Unknown direction!")
+                    }
+                    break;
+                case CommandType.RotateRight:
+                    break;
+                case CommandType.RotateLeft:
+                    break;
+                default:
+                    throw new Error("Command type not known!");
+            }
         }
 
         return this.coordinates.toString();
