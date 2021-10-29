278a135df9000d65b4f5efcdd91711c59e3397b2
Peter Popov <p.dim.popov@gmail.com>
2021-10-29 12:16:16 +0300

    feat(ControlButton): implement click handler forward

--- src/components/ControlButton/ControlButton.test.tsx
+++ src/components/ControlButton/ControlButton.test.tsx
@@ -1,5 +1,6 @@
 import {render, screen} from "@testing-library/react";
 import ControlButton from "./ControlButton";
+import userEvent from "@testing-library/user-event";
 
 describe("ControlButton", () => {
     it('should have rounded borders, gray background and black color', function () {
@@ -12,5 +13,16 @@ describe("ControlButton", () => {
         expect(button?.classList.contains("text-black")).toBeTruthy();
         expect(button?.classList.contains("rounded")).toBeTruthy();
         expect(button?.classList.contains("m-2")).toBeTruthy();
+    });
+
+    it('should have onClick handler', function () {
+        const onClickMock = jest.fn();
+        render(<ControlButton onClick={onClickMock}>TEST</ControlButton>);
+
+        const button = screen.getByText("TEST");
+
+        userEvent.click(button);
+
+        expect(onClickMock).toBeCalled();
     });
 })
--- src/components/ControlButton/ControlButton.tsx
+++ src/components/ControlButton/ControlButton.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 export interface IControlButtonProps {
     children: string;
+    onClick?: Function;
 }
 
-const ControlButton: React.FC<IControlButtonProps> = ({ children }) => (
-    <button className="bg-gray-300 text-black rounded m-2">{children}</button>
+const ControlButton: React.FC<IControlButtonProps> = ({ children, onClick }) => (
+    <button className="bg-gray-300 text-black rounded m-2" onClick={(_) => onClick?.()}>{children}</button>
 );
 
 export default ControlButton;
