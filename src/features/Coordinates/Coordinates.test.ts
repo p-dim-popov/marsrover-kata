4db1081ceacdf598d53178af3ae2ffaf51266c2f
Peter Popov <p.dim.popov@gmail.com>
2021-10-29 19:05:45 +0300

    refactor(utils): move stringifyCoordinates to tests

--- src/features/Coordinates/Coordinates.test.ts
+++ src/features/Coordinates/Coordinates.test.ts
@@ -1,5 +1,17 @@
 import {Coordinates} from "./Coordinates";
-import {stringifyCoordinates} from "../utils";
+
+const stringifyCoordinates = (row: number, col: number, direction: string, hasObstacles: boolean | string) => {
+    let result = `${row}:${col}:${direction}`;
+    if (hasObstacles) {
+        result = "O:" + result;
+    }
+
+    if (typeof hasObstacles === "string") {
+        result = result.replace("O:", hasObstacles);
+    }
+
+    return result;
+}
 
 describe("Coordinates", () => {
     describe("parse", () => {
@@ -20,7 +32,7 @@ describe("Coordinates", () => {
             [1, 2, "", true],
             [1, 2, "", "Z:"],
         ])('should throw (%s:%s:%s), with obstacles - %s', function (row?: number, col?: number, direction?: string, hasObstacles?: boolean | string) {
-            const stringCoordinates = stringifyCoordinates(row, col, direction, hasObstacles);
+            const stringCoordinates = stringifyCoordinates(row!, col!, direction!, hasObstacles!);
             expect(() => Coordinates.parse(stringCoordinates)).toThrow();
         });
     })
--- src/features/utils.ts
+++ src/features/utils.ts
@@ -4,24 +4,6 @@ export const isNullNanUndefinedOrEmptyString = (value: any): boolean => {
     return value === null || value === void 0 || Number.isNaN(value) || value === "";
 }
 
-export const stringifyCoordinates = (row?: number, col?: number, direction?: string, hasObstacles?: boolean | string) => {
-    let result = `${row}:${col}:${direction}`;
-    switch (typeof hasObstacles) {
-        case "boolean":
-            if (hasObstacles) {
-                result = "O:" + result;
-            }
-            break;
-        case "string":
-            result = hasObstacles + result;
-            break;
-        default:
-            break;
-    }
-
-    return result;
-}
-
 export const useForceUpdate = () => {
     const [x, setX] = useState(1);
     return () => setX(x + 1);
