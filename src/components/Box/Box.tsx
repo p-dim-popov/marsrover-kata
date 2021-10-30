94fe8ee6b7d79d6716f0d58b00b7d2f65c12d5a7
Peter Popov <p.dim.popov@gmail.com>
2021-10-30 16:26:33 +0300

    feat(Box): add different bg colors to different types

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -11,8 +11,15 @@ export interface IBoxProps {
     type: BoxType,
 }
 
+const ColorsMap: Record<BoxType, string> = {
+    [BoxType.Rover]: "blue",
+    [BoxType.NotVisited]: "gray",
+    [BoxType.Obstacle]: "red",
+    [BoxType.Visited]: "green",
+}
+
 const Box: React.FC<IBoxProps> = ({ type, ...rest }) => {
-    return <div className="p-2 h-10 w-10 bg-gray-900 rounded-full flex items-center justify-center" {...rest} >{type}</div>;
+    return <div className={`p-2 h-10 w-10 bg-${ColorsMap[type]}-900 rounded-full flex items-center justify-center`} {...rest} >{type}</div>;
 }
 
 export default Box;
